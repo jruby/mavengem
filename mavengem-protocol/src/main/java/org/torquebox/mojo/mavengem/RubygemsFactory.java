ef59a8423ed4a24f705edb3d46cef952e669cb3c
Christian Meier <meier.kristian@gmail.com>
2016-06-11 11:58:31 +0200

    cleanup

--- mavengem-protocol/src/main/java/org/torquebox/mojo/mavengem/RubygemsFactory.java
+++ mavengem-protocol/src/main/java/org/torquebox/mojo/mavengem/RubygemsFactory.java
@@ -1,33 +1,15 @@
 package org.torquebox.mojo.mavengem;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.jruby.embed.IsolatedScriptingContainer;
-import org.jruby.embed.ScriptingContainer;
-import org.sonatype.nexus.ruby.DefaultRubygemsGateway;
-import org.sonatype.nexus.ruby.RubygemsGateway;
-import org.sonatype.nexus.ruby.FileType;
-import org.sonatype.nexus.ruby.GemArtifactFile;
-import org.sonatype.nexus.ruby.IOUtil;
-import org.sonatype.nexus.ruby.RubygemsFile;
-import org.sonatype.nexus.ruby.cuba.RubygemsFileSystem;
-import org.sonatype.nexus.ruby.layout.CachingProxyStorage;
-import org.sonatype.nexus.ruby.layout.ProxiedRubygemsFileSystem;
-import org.sonatype.nexus.ruby.layout.ProxyStorage;
-
 public class RubygemsFactory {
 
-    private static RubygemsGateway gateway = new DefaultRubygemsGateway(new IsolatedScriptingContainer());
-
-    private static Map<URL, Rubygems> facades = new HashMap<URL, Rubygems>();
-    static URL NO_MIRROR;
+    private static final Map<URL, Rubygems> facades = new HashMap<>();
+    static final URL NO_MIRROR;
     static {
 	try {
 	    NO_MIRROR = new URL("http://example.com/no_mirror");
@@ -55,33 +37,27 @@ public class RubygemsFactory {
 	return factory;
     }
 
-    public RubygemsFactory()
-	    throws MalformedURLException {
+    public RubygemsFactory() throws MalformedURLException {
 	this(DEFAULT_CACHEDIR, NO_MIRROR, null);
     }
 
-    public RubygemsFactory(URL mirror)
-	    throws MalformedURLException {
+    public RubygemsFactory(URL mirror) throws MalformedURLException {
 	this(DEFAULT_CACHEDIR, mirror, null);
     }
 
-    public RubygemsFactory(Map<URL, URL> mirrors)
-	    throws MalformedURLException {
+    public RubygemsFactory(Map<URL, URL> mirrors) throws MalformedURLException {
 	this(DEFAULT_CACHEDIR, NO_MIRROR, mirrors);
     }
 
-    public RubygemsFactory(File cacheDir)
-	    throws MalformedURLException {
+    public RubygemsFactory(File cacheDir) throws MalformedURLException {
 	this(cacheDir, NO_MIRROR, null);
     }
 
-    public RubygemsFactory(File cacheDir, URL mirror)
-	    throws MalformedURLException {
+    public RubygemsFactory(File cacheDir, URL mirror) throws MalformedURLException {
 	this(cacheDir, mirror, null);
     }
 
-    public RubygemsFactory(File cacheDir, Map<URL, URL> mirrors)
-	    throws MalformedURLException {
+    public RubygemsFactory(File cacheDir, Map<URL, URL> mirrors) throws MalformedURLException {
 	this(cacheDir, NO_MIRROR, mirrors);
     }
 
@@ -111,7 +87,7 @@ public class RubygemsFactory {
 		this.catchAllMirror = NO_MIRROR;
 	    }
 	}
-	this.mirrors = mirrors == null ? null : new HashMap(mirrors);
+	this.mirrors = mirrors == null ? null : new HashMap<>(mirrors);
 	if (cacheDir != null) {
 	    this.cacheDir = cacheDir;
 	}
@@ -133,7 +109,7 @@ public class RubygemsFactory {
 	}
 
 	// FIXME the cachedir when coming from the facades map
-	//       can be different as map get shared between factories
+	// FIXME can be different as map get shared between factories
 	synchronized(facades) {
 	    Rubygems result = facades.get(url);
 	    if (result == null) {
